7bf320a420c38d1f5248f79424ee7c57109e35fd
Henrique <henrique.dias@gruponewway.com.br>
2023-02-13 19:46:55 -0300

    chore: update all

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -1,18 +1,21 @@
 import MessageDto from "../MessageDto"
+import { toFormatedDate } from "../Utils"
 import './Message.css'
 
 export default function Message(props){
   const { socket } = props
   const data: MessageDto = props.data
   const isYou: boolean = data.id === socket.id ? true : false
-
+  
   return <div className={`message-container ${isYou === true ? 'you' : 'other'}`}>
     <div className={`message-wrapper `}>
       <div className="message-header">{isYou === true  ? '' : data.username}</div>
       <div className="message-body">{data.message}</div>
       <div className="message-footer">
         <div className="room">{data.room}</div>
-        <div className="created-at">{data.createdAt}</div>
+        <div className="created-at">
+          {toFormatedDate(data.createdAt)}
+        </div>
       </div>
     </div>
   </div>
